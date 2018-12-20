5506625b759880265fbf19bddfd75f1543675a99
Anna-Mariia Shkarlinska <anna-mariia.shkarlinska@netguru.co>
2018-12-20 17:30:49 +0100

    Initial setup

---
+++ CarlensCollectionViewLayout/CarlensCollectionViewLayout/CarlensCollectionViewLayout.h
@@ -0,0 +1,19 @@
+//
+//  CarlensCollectionViewLayout.h
+//  CarlensCollectionViewLayout
+//
+//  Created by Anna on 20/12/2018.
+//  Copyright © 2018 Netguru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarlensCollectionViewLayout.
+FOUNDATION_EXPORT double CarlensCollectionViewLayoutVersionNumber;
+
+//! Project version string for CarlensCollectionViewLayout.
+FOUNDATION_EXPORT const unsigned char CarlensCollectionViewLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarlensCollectionViewLayout/PublicHeader.h>
+
+
