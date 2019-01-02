688272e91d0489329d10fcf69aa4d8845c138c9c
Anna-Mariia Shkarlinska <anna-mariia.shkarlinska@netguru.co>
2019-01-02 18:46:03 +0100

    Reorder properties in the cell

--- CarlensCollectionViewLayout/CarlensCollectionViewCell.swift
+++ CarlensCollectionViewLayout/CarlensCollectionViewCell.swift
@@ -11,14 +11,14 @@ open class CarlensCollectionViewCell: UICollectionViewCell {
     /// Indicates if the cell is currently displayed as primary cell.
     public var isCurrentlyPrimary = false
     
-    private let topViewHeight: CGFloat = UIScreen.main.bounds.height > 568 ? 200 : 170
-    
     /// The upper view of the cell.
     public var topView: UIView!
     
     /// The bottom view of the cell.
     public var cardView: UIView!
 
+    private let topViewHeight: CGFloat = UIScreen.main.bounds.height > 568 ? 200 : 170
+    
     /// Configuration of the cell. Must be called on a start.
     ///
     /// - Parameters:
