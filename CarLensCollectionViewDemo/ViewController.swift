8e9d4627090b7fbe35bf32dbcdf3e1109cc61b0f
Anna-Mariia Shkarlinska <anna-mariia.shkarlinska@netguru.co>
2019-01-08 16:11:41 +0100

    Add initial demo project

---
+++ CarLensCollectionViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Anna on 08/01/2019.
+//  Copyright © 2019 Netguru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
